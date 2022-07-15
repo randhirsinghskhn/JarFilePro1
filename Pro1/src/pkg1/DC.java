9711f787c89febf886b8cc31df2e7bc55a90b088
randh <randh@TABLET-336T4SBR>
2022-07-15 19:04:51 +0530

    first commit.

---
+++ Pro1/src/pkg1/DC.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public class DC {
+
+	public void m1()
+	{
+		System.out.println("Project 1 method 1");
+	}
+	public void m2()
+	{
+		System.out.println("Project 1 method 2");
+	}
+}
